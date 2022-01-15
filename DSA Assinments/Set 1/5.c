7caa8634ea65d38ce9a07aaddc96ddc03f944c68
Koushik Mahanta <77391839+Koushik0001@users.noreply.github.com>
2022-01-15 14:58:58 +0530

    just do it

--- DSA Assinments/Set 1/5.c
+++ DSA Assinments/Set 1/5.c
@@ -22,29 +22,50 @@ void DeletionAtposition(struct node**,int);
 int number_of_nodes(struct node**);
 void Rev_list(struct node**,struct node*);
 void display(struct node**);
+void insert_befoe_last(struct node** head,struct node* new);
+void print_common(struct node* head1, struct node* head2);
 int main()
 {
-    struct node *head = (struct node*)malloc(sizeof(struct node));
-    struct node **ptrheadptr = (struct node**)malloc(sizeof(struct node*));
-    head = NULL;
-    ptrheadptr = &head;
-    for(int i=0; i<5; i++)
-        InsertionAtend(ptrheadptr);
-    display(ptrheadptr);
-    InsertionAtbeginning(ptrheadptr);
-    display(ptrheadptr);
-    printf("\nEnter the position number where you want insrt the node(index starts from 1) : ");
-    int position;
-    scanf("%d",&position);
-    InsertionAtposition(ptrheadptr,position);
-    display(ptrheadptr);
-    printf("\nDelete n th node(index starts from 1) where n =  ");
-    scanf("%d",&position);
-    DeletionAtposition(ptrheadptr,position);
-    display(ptrheadptr);
-    printf("\nThe number of nodes in linked list is %d\n",number_of_nodes(ptrheadptr));
-    printf("The reversed linked list is : ");
-    Rev_list(ptrheadptr,NULL);
+    struct node *head1 = (struct node*)malloc(sizeof(struct node));
+    struct node *head2 = (struct node*)malloc(sizeof(struct node));
+    struct node **ptrheadptr1 = (struct node**)malloc(sizeof(struct node*));
+    struct node **ptrheadptr2 = (struct node**)malloc(sizeof(struct node*));
+    head1 = NULL;
+    head2 = NULL;
+    ptrheadptr1 = &head1;
+    ptrheadptr2 = &head2;
+    char ch;
+    do
+    {
+        printf("enter number of nodes : ");
+        int num;
+        scanf("%d",&num);
+        for(int i=0; i<num; i++)
+            InsertionAtend(ptrheadptr1);
+        printf("enter next list : ");
+        for(int i=0; i<num+2; i++)
+            InsertionAtend(ptrheadptr2);
+        print_common(head1,head2);
+        /*display(ptrheadptr);
+        InsertionAtbeginning(ptrheadptr);
+        display(ptrheadptr);
+        printf("\nEnter the position number where you want insrt the node(index starts from 1) : ");
+        int position;
+        scanf("%d",&position);
+        InsertionAtposition(ptrheadptr,position);
+        display(ptrheadptr);
+        printf("\nDelete n th node(index starts from 1) where n =  ");
+        scanf("%d",&position);
+        DeletionAtposition(ptrheadptr,position);
+        display(ptrheadptr);
+        printf("\nThe number of nodes in linked list is %d\n",number_of_nodes(ptrheadptr));
+        printf("The reversed linked list is : ");
+        Rev_list(ptrheadptr,NULL);*/
+        free(head1);
+        free(head2);
+        head1 = NULL;
+        head2 = NULL;
+    }while(ch != '\n');
 }
 struct node* Traversal(struct node** ptrheadptr)
 {    
@@ -162,5 +183,47 @@ void display(struct node** ptrheadptr)
             printf("%d  ",temp->data);
             temp = temp->next;
         }
+    }
+}
+/*1. Write an algorithm to inset a new node before the last node of a linked list 
+using a single additional pointer. (10)*/
+void insert_befoe_last(struct node** head,struct node* new)
+{
+    struct node* temp = *head;
+    if(temp->next == NULL )
+    {
+        new->next = temp; 
+        *head = new;
+    }
+    else
+    {
+        while(temp->next->next != NULL)
+            temp = temp->next;
+        new->next = temp->next;
+        temp->next = new;
+    }
+}
+void print_common(struct node* head1, struct node* head2)
+{
+    struct node* temp1 = head1, *temp2 = head2;
+     int key = temp1->data;
+    while(temp1 != NULL && temp2 != NULL)
+    {
+        if(temp2->data == key)
+        {
+            printf("%d ",key);
+            while(key == temp2->data)
+                temp2 = temp2->next;
+            while (key == temp1->data)
+                temp1 = temp1->next;
+            key = temp1->data;
+        }
+        while(key < temp2->data)
+        {
+            temp1 = temp1->next;
+            key = temp1->data;
+        }
+        while(key > temp2->data)
+            temp2 = temp2->next;
     }
 }
